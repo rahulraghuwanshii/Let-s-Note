825a69dfc575575ccc8559b2e8fcc370695e3484
Rahul Raghuwanshi <champrahul202002@gmail.com>
2023-02-28 10:58:24 +0530

    * NotesScreen completed.

--- app/src/main/java/com/rahulraghuwanshi/letsnote/feature_notes/presentation/MainActivity.kt
+++ app/src/main/java/com/rahulraghuwanshi/letsnote/feature_notes/presentation/MainActivity.kt
@@ -3,13 +3,16 @@ package com.rahulraghuwanshi.letsnote.feature_notes.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 import com.rahulraghuwanshi.letsnote.ui.theme.LetsNoteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LetsNoteTheme {}
+            LetsNoteTheme {
+                Text(text = "Hello this is my first compose text")
+            }
         }
     }
 }
